6396ba5ec9d8f8fcd0b9f2ee0ea1ccfd8b7ed1d1
Kevin Talbot <kevintalbot17@gmail.com>
2015-06-07 12:28:39 +0100

    further updates to the BallDemo.java file to add in the random placement of balls and their size

--- bouncing-balls/BallDemo.java
+++ bouncing-balls/BallDemo.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.awt.geom.*;
 import java.util.ArrayList;
 import java.lang.Object;
+import java.util.Random;
 
 /**
  * Class BallDemo - provides two short demonstrations showing how to use the 
@@ -79,28 +80,33 @@ public class BallDemo
     {
         // example array construction
         // Student[] studentArray = new Student[7];
-        
         BouncingBall[] ballArray = new BouncingBall[ballNumber];
         int i;
-        
         int ground = 400;   // position of the ground line
-
         myCanvas.setVisible(true);
 
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
         
-        int xAxis = 30;
+        // generating random numbers here
+        // want to generate 2 random numbers for each new BallArray object
+        
+
+        
         // create and show the balls
         for(i = 0;i < ballNumber;i++)
         {
-            if(xAxis == xAxis)
-            {
-                xAxis = xAxis + 20;
-            }
-            ballArray[i] = new BouncingBall(xAxis, 50, 16, Color.blue, ground, myCanvas);
+            Random rand = new Random();
+            int rNumber = rand.nextInt(300);
+            int xAxis = rNumber;
+            int rNumber1 = rand.nextInt(400);
+            int yAxis = rNumber1;
+            int rNumber2 = rand.nextInt(32);
+            int size = rNumber2;
+            ballArray[i] = new BouncingBall(xAxis, yAxis, size, Color.blue, ground, myCanvas);
             ballArray[i].draw();
         }
+        
         // make them bounce
         boolean finished =  false;
         while(!finished) {
@@ -113,8 +119,11 @@ public class BallDemo
             // stop once ball has travelled a certain distance on x axis
             if(ballArray[0].getXPosition() >= 550) {
                 finished = true;
+                for(i=0;i < ballNumber; i++)
+                {
+                    ballArray[i].erase();
+                }
             }
         }
-        ballArray[i].erase();
     }
 }
