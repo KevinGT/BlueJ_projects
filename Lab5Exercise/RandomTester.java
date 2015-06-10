fee700951e3cc886cd85d3978741e76304efc21c
Kevin Talbot <kevintalbot17@gmail.com>
2015-06-10 11:39:01 +0100

    adding both files RandomTester.class and RandomTester.java to incorporate more sophisticated answer from Ron Grau on the getResponse2() method

--- Lab5Exercise/RandomTester.java
+++ Lab5Exercise/RandomTester.java
@@ -65,14 +65,23 @@ public class RandomTester
              System.out.println("Maybe");
             }
        
+            
+       // NOTE: this example method above could have been completed by creating an array of answers
    }
    
    public String getResponse2()
    {
+       // the correct succingt solution to this method from Dr Ron Grau
+       // since we do not manipulate the responses.size in any way we can insert this directly as
+       // the input for responses.get()
+       // *remembering of course that if there are 3 items in our array that the maximum answer 
+       // from the index is 3. This being stored as index value 0, 1, 2.
+       return responses.get(number.nextInt(responses.size()));
+       
        
        // create a random number that represent the index in the list ofpossible responses
-       int index = number.nextInt(responses.size()-1);
-       return responses.get(index);
+       // int index = number.nextInt(responses.size()-1);
+       // return responses.get(index);
        
    }
 }
